60addc6496ae34afebf8f19b80172636dd499996
kymdennis3 <kymdennis3@gmail.com>
2022-11-01 07:08:12 +0300

    Introduced the NOT(!) and AND(&&) operators

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*Logical operators = used to connect two or more expressions
+             && = (AND) both conditions must be true
+             || = (OR) Either condition must be true
+             ! = (NOT) reverses boolean value of a condition
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game");
+
+        String input = scanner.nextLine();
+
+        if (!input.equals("q") && !input.equals("Q")) {
+            System.out.println("You are still playing the game");
+        }
+        else {
+            System.out.println("You have exited the game");
+        }
+
     }
 }
